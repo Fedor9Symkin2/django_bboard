9c10e255364834e918de6755c0977406cdb78e05
Nikolay Savenko <nikolay.savenko.92@yandex.ru>
2021-11-07 12:37:04 +0300

    add user

--- main/urls.py
+++ main/urls.py
@@ -1,12 +1,14 @@
 from django.urls import path
-from .views import index, other_page
+from .views import index, other_page, BBLoginView, BBLogoutView, profile, ChangeUserInfoView, BBPasswordChangeView
 
 app_name = 'main'
 
 urlpatterns = [
+    path('accounts/password/change/', BBPasswordChangeView.as_view(), name='password_change'),
+    path('accounts/profile/change/', ChangeUserInfoView.as_view(), name='profile_change'),
+    path('accounts/logout/',BBLogoutView.as_view(), name='logout'),
+    path('accounts/profile/', profile, name='profile'),
+    path('account/login/', BBLoginView.as_view(), name='login'),
     path('<str:page>/', other_page, name='other'),
     path('', index, name='index'),
 ]
-"""
-commit
-"""
